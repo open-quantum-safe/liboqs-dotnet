1cbedd70155aa4b639fe9f5643afe2d39a7ceca2
Christian Paquin <cpaquin@microsoft.com>
2022-01-05 15:55:56 -0500

    Update .net version

--- scripts/linux_macos_wrapper_build.sh
+++ scripts/linux_macos_wrapper_build.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard1.2 -c Debug -o bin/Debug/dotnetOQS-netstandard1.2
-dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.0 -c Debug -o bin/Debug/dotnetOQS-netstandard2.0
-dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard1.2 -c Release -o bin/Release/dotnetOQS-netstandard1.2
-dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.0 -c Release -o bin/Release/dotnetOQS-netstandard2.0
+dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.1 -c Debug -o bin/Debug/dotnetOQS-netstandard2.1
+dotnet build dotnetOQS/dotnetOQS.csproj /p:Platform=AnyCPU -f netstandard2.1 -c Release -o bin/Release/dotnetOQS-netstandard2.1
